27736954d477ece643f7e4e391d14a9d8401576a
sameershaikh1246 <srrahman07498@gmail.com>
2025-04-27 23:40:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "17950436"
 api_hash = "9d8ae4d35b1106df910b94dcaf73d346"
 bot_token = "7799691566:AAFqgN3_1mFw1HnZApJ18zJhiYFepcOj-Pc"
-auth_users = [7615363859]
+auth_users = [7615363859,5500536251]
