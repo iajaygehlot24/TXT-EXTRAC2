aed34e27277f5e1b675e921bba0ba0f3ebf15384
sameershaikh1246 <srrahman07498@gmail.com>
2025-04-27 23:10:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ async def start(bot, message):
 
   await message.reply_photo(
     photo=random_image_url,
-    caption="**Developer - @pwextractowner\nPLEASE👇PRESS👇HERE**",
+    caption="**Developer - @CHAT_WITH_SAMEER_BOT\nPLEASE👇PRESS👇HERE**",
     quote=True,
     reply_markup=reply_markup
   )
