587b83c137d51a46268fc2eb7da3aada0557dfa2
SAMEER JI <srrahman07498@gmail.com>
2025-04-29 10:20:56 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "SAMEER JI ❤️ Flask Server Working on Port 8080!"
+def hello_world():
+    return "Ankit Shakya"
+
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run()
